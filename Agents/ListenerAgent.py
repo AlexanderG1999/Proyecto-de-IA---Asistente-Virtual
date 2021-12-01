1720352593dcb4e7d8f851c486491d2aa41c9f55
Leo_Andrade <leomijail@hotmail.com>
2021-12-01 08:33:43 -0500

    prueba2

--- Agents/ListenerAgent.py
+++ Agents/ListenerAgent.py
@@ -7,7 +7,7 @@ def listen(listener, nameAV):
             print("Escuchando...")
             pc = listener.listen(source)
             rec = listener.recognize_google(pc)
-            #rec = rec.lower()
+            rec = rec.lower()
             
             if nameAV in rec:
                 rec = rec.replace(nameAV, '')
