33df41bb37774c8c4815518e62fe9c1007b1f455
Leo_Andrade <leomijail@hotmail.com>
2022-01-04 23:59:37 -0500

    function

--- Pruebas/mail.py
+++ Pruebas/mail.py
@@ -1,16 +1,17 @@
 import smtplib
 
+def sendMessage():
 
-message = "Hola, un mensaje desde Python!\n ya valio papu, mi IA envia correos"
-subject = "Prueba de correo"
+    message = "Hola, un mensaje desde Python!\n ya valio papu, mi IA envia correos"
+    subject = "Prueba de correo"
 
-message = 'Subject: {}\n\n{}'.format(subject,message)
+    message = 'Subject: {}\n\n{}'.format(subject,message)
 
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login('leo.andrade.la1@gmail.com','10191999')
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('leo.andrade.la1@gmail.com','10191999')
 
-server.sendmail('leo.andrade.la1@gmail.com','veronica99quesada@gmail.com', message)
-server.quit()
+    server.sendmail('leo.andrade.la1@gmail.com','veronica99quesada@gmail.com', message)
+    server.quit()
 
-print("Correo enviado exito")
+    print("Correo enviado exito")
