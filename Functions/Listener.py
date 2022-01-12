ead73fa43cc04412bc03e1334182c6f345fc3784
Leo_Andrade <leomijail@hotmail.com>
2022-01-12 12:36:20 -0500

    serv. Audio

--- Functions/Listener.py
+++ Functions/Listener.py
@@ -2,18 +2,22 @@ import speech_recognition as sr
 import Functions.Talker
 
 # AV escuche
+
+
 def listen(listener, nameAV, engine):
     try:
         with sr.Microphone() as source:
             print("Escuchando...")
             Functions.Talker.talk("Te escucho", engine)
+            listener.adjust_for_ambient_noise(source)
             pc = listener.listen(source)  # Escuchar desde el microfono
-            rec = listener.recognize_google(pc) # Utilizar servicios de reconocimiento de Google
+            # Utilizar servicios de reconocimiento de Google
+            rec = listener.recognize_google(pc, language='es-Mx')
             rec = rec.lower()
-            
+
             if nameAV in rec:
                 rec = rec.replace(nameAV, '')
-                
+
     except:
         pass
     return rec
