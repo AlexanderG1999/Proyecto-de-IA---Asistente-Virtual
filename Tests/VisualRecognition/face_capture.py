9fde5f8991ea90b7030903950842a93dc2b5d873
Leo_Andrade <leomijail@hotmail.com>
2022-01-11 20:36:20 -0500

    VisualRecog.. 11/01/2022

--- Tests/VisualRecognition/face_capture.py
+++ Tests/VisualRecognition/face_capture.py
@@ -9,10 +9,10 @@ person_path = data_path + '/' + person
 if not os.path.exists(person_path):
     os.makedirs(person_path)
 
-capture = cv2.VideoCapture('C:/Users/ASUS/OneDrive/Escritorio/EPN/5toSemestre/InteligenciaArtificial/Proyecto IA - Asistente Virtual/Videos/Alexander.mp4')  # 0 tu propia camara, #1 camara remota, path.mp4
+capture = cv2.VideoCapture(0)  # 0 tu propia camara, #1 camara remota, path.mp4
 
-'''face_classif = cv2.CascadeClassifier('C:/Users/leomi/Documents/Python/ReconocimientoFacial/Entrenamiento Ruido/opencv-4.x/data/haarcascades/haarcascade_frontalface_default.xml')'''
-face_classif = cv2.CascadeClassifier('C:/Users/ASUS/OneDrive/Escritorio/EPN/5toSemestre/InteligenciaArtificial/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
+
+face_classif = cv2.CascadeClassifier('Ruido/haarcascade_frontalface_default.xml')
 count = 0
 
 while True:
@@ -25,19 +25,19 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     aux_frame = frame.copy()
 
-    faces = face_classif.detectMultiScale(gray, 1.3, 2)
+    faces = face_classif.detectMultiScale(gray, 1.5, 2)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)  # dibuja rectangulo en el rostro
-        face = aux_frame[y:y+h, x:x+w]
-        face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(person_path+f'/face_{count}.jpg', face)
-        count += 1
+        #face = aux_frame[y:y+h, x:x+w] #recoleccion de data
+        #face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)
+        #cv2.imwrite(person_path+f'/face_{count}.jpg', face)
+        #count += 1
     
     cv2.imshow('frame', frame)
     key = cv2.waitKey(1)
     
-    if key == 27 or count >= 300 or key == ord('s'):
+    if key == ord('s'): #or key == 27 or count >= 300:
         break
 
 capture.release()
