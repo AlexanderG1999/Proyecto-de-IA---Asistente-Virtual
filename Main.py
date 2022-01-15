1e7059766399f42e64a3f6cfa5a126d21d2e3b7e
AlexanderG1999 <95202925+AlexanderG1999@users.noreply.github.com>
2022-01-14 22:54:11 -0500

    SpeechRecognition mejorado con VirtualSara2.py 14-01-2022

--- Main.py
+++ Main.py
@@ -1,14 +1,20 @@
+from errno import ENFILE
 import speech_recognition as sr
 import pyttsx3
 
-from Functions.VirtualSara import runSara
+#from Functions.VirtualSara import runSara
+from Functions.Listener import listen
+from Tests.VirtualSara2 import runSara
 
-name = "Sara"
-listener = sr.Recognizer()
+name = 'sara'
+#listener = sr.Recognizer()
+#listener.energy_threshold = 5000
+#listener.dynamic_energy_threshold = False
 engine = pyttsx3.init()  # Transforma texto a voz
 voices = engine.getProperty('voices')# Se escoge una de las voces del sistema (Español)
 engine.setProperty('voice', voices[0].id)
 
 # Main
 if __name__ == '__main__':
-    runSara(listener, name, engine)
+    runSara(name, engine)
+    #listen(name, engine)
